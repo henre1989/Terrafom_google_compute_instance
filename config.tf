986c5529fec26e1b3de09f31d125e7ecb7bb1778
henre1989 <transfer1989@gmail.com>
2021-03-09 17:58:07 +0500

    Initial commit

---
+++ config.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.59.0"
+    }
+  }
+}
+
+provider "google" {
+  resource "google_compute_instance" "default" {
+  name         = "build_ist"
+  machine_type = "e2-medium"
+  zone         = "europe-north1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu--2004-lts"
+    }
+  }
+}
